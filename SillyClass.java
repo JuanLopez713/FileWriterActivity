b638ed1fa465e492dddcb1ff7046885481455b5e
Juan Lopez <juanlpz713@gmail.com>
2024-09-06 14:43:42 -0700

    Fixed silly message
    No longer silly

--- SillyClass.java
+++ SillyClass.java
@@ -1,6 +1,6 @@
 public class SillyClass {
     public static void main(String[] args) {
         System.out.println("This is a silly class! ");
-        System.out.println("this sure is a silly class!");
+        System.out.println("this sure is NOT a silly class!");
     }
 }
